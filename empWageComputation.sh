007b790096121f72eaa781af1f13732c7c70a792
neelrahulr <rathorerahul131@gmail.com>
2020-11-21 12:08:06 +0530

    Refactored: Monthly Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -13,11 +13,14 @@ else
 	echo "Employee is Present"
 fi
 
-# Calculating and Displaying Daily Wage of the Employee using Switch Case
+# Calculating and Displaying Daily-Wage and the Monthly-Wage of the Employee using Switch Case
 
 wagePerHour=20
+
 fullDayHour=8
 partTimeHour=4
+
+daysPerMonth=20
 
 isPartTime=1;
 isFullTime=2;
@@ -27,15 +30,24 @@ randomCheck=$(($((RANDOM%2))+1));
 case $randomCheck in
 	$isPartTime)
 			DailyWage=$(( $wagePerHour * $partTimeHour ))
-			echo "The employee is working Part Time so his wage will be $DailyWage"
+			monthlyWage=$(( $DailyWage * $daysPerMonth ))
+
+			echo "The employee is working Part Time so his Daily-Wage will be $DailyWage" 
+			echo "and the Monthly-Wage will be $monthlyWage"
 			;;
 	$isFullTime)
 			DailyWage=$(( $wagePerHour * $fullDayHour ))
-			echo "The employee is working Full Day so his wage will be $DailyWage"
+			monthlyWage=$(( $DailyWage * $daysPerMonth ))
+
+			echo "The employee is working Full Day so his Daily-Wage will be $DailyWage" 
+			echo "and the Monthly-Wage will be $monthlyWage"
 			;;
 	*)
 		echo "Employee Unidentified"
 esac
+
+
+
 
 
 
