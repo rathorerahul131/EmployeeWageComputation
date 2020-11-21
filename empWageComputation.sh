f25eec6b2535226b6d941e4d45d9fbf6a940aee9
neelrahulr <rathorerahul131@gmail.com>
2020-11-21 16:26:16 +0530

    Refactored: Stored Daily Wages and Total Wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -15,60 +15,58 @@ fi
 
 # Calculating and Displaying Daily-Wage and the Monthly-Wage of the Employee using Switch Case
 
-wagePerHour=20
+WAGE_PER_HOUR=20
 
-fullDayHour=8
-partTimeHour=4
+FUll_DAY_HOUR=8
+PART_TIME_HOUR=4
 
-daysPerMonth=20
+DAYS_PER_MONTH=20
 
-isPartTime=1;
-isFullTime=2;
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
 
-maxHoursInMonth=100
+MAX_HOURS_IN_MONTH=100
+
 totalHours=0
 maxWorkDaysInMonth=0
 
 #function that returns the Work Time in Hours as per Part Time and Full Time
 function getWorkHours(){
 	case $1 in
-		$isPartTime)
-				WorkTimeInHour=4
-				DailyWage=$(( $wagePerHour * $partTimeHour ))
-				monthlyWage=$(( $DailyWage * $daysPerMonth ))
+		$IS_PART_TIME)
+				WORK_TIME_IN_HOURS=4
 				;;
-		$isFullTime)
-				WorkTimeInHour=8
-				DailyWage=$(( $wagePerHour * $fullDayHour ))
-				monthlyWage=$(( $DailyWage * $daysPerMonth ))
+		$IS_FULL_TIME)
+				WORK_TIME_IN_HOURS=8
 				;;
 		*)
 				echo "Employee Unidentified"
 	esac
-	echo $WorkTimeInHour
+	echo $WORK_TIME_IN_HOURS
 }
 
 # function to get per day wage of employee
 function getEmpWages(){
 	empHours=$1
-	echo $(( $WorkTimeInHour * $wagePerHour ))
+	echo $(( $WORK_TIME_IN_HOURS * $WAGE_PER_HOUR ))
 }
 
 # loop till total hours has reached 100 or max working days In month has reached 20
-while [[ $totalHours -le $maxHoursInMonth && $maxWorkDaysInMonth -le $daysPerMonth ]]
+while [[ $totalHours -le $MAX_HOURS_IN_MONTH && $maxWorkDaysInMonth -le $DAYS_PER_MONTH ]]
 do
 	((maxWorkDaysInMonth++))
-	WorkTimeInHour=$( getWorkHours $(($((RANDOM%2))+1)) )
-	totalHours=$(( $totalHours + $WorkTimeInHour ))
-	dailyWages[$maxWorkDaysInMonth]=$( getEmpWages $WorkTimeInHour)
+	WORK_TIME_IN_HOURS=$( getWorkHours $(($((RANDOM%2))+1)) )
+	totalHours=$(( $totalHours + $WORK_TIME_IN_HOURS ))
+	dailyWages["Day"$maxWorkDaysInMonth]=$( getEmpWages $WORK_TIME_IN_HOURS ) 
 done
 
-totalSalary=$(( $totalHours * $wagePerHour ))
+totalSalary=$(( $totalHours * $WAGE_PER_HOUR ))
 
 echo "Total working days are $maxWorkDaysInMonth and working hours are $totalHours "
 echo "Total salary : $totalSalary"
 
-echo "DailyWages: ${dailyWages[@]}"
+echo DailyWages:${dailyWages[@]}	#prints the Daily wages 
+echo Day:${!dailyWages[@]}		#print the days
 
 
 
