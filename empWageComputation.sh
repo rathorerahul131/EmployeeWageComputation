f0eee86b422e1c48585c60bca1b9d4e945c42532
neelrahulr <rathorerahul131@gmail.com>
2020-11-21 11:08:08 +0530

    Refactored: Attendance Check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,5 +2,13 @@
 
 echo "Welcome to the Employee Wage Computation Program"
 
- 	
+empAttendance=$(( $RANDOM % 2 )) #Generates 0 or 1 randomly and assign it to the empAttendance variable
+
+#Conditon for Attendance Check
+if [[ $empAttendance -eq 0 ]]
+then
+	echo "Employee is Absent"
+else
+	echo "Employee is Present"
+fi
 
